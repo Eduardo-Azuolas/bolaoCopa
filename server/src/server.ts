5fa224ead38a0e06deb0fe0b2d4b40d1e5144ef2
Eduardo Azuolas <eduardoazuolas@gmail.com>
2022-11-07 15:35:31 -0300

    Finnishing project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,33 +1,38 @@
 import Fastify from 'fastify';
-import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
-
+import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+import { PrismaClient } from '@prisma/client';
 import { poolRoutes } from './routes/pool';
-import { authRoutes } from './routes/auth';
-import { gameRoutes } from './routes/game';
-import { guessRoutes } from './routes/guess';
 import { userRoutes } from './routes/user';
+import { guessRoutes } from './routes/guess';
+import { gameRoutes } from './routes/game';
+import { authRoutes } from './routes/auth';
 
-async function bootstrap() {
-  const fastify = Fastify({
-    logger: true,
-  })
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
-  await fastify.register(cors, {
-    origin: true,
-  })
+async function boostrap() {
+    const fastify = Fastify({
+        logger: true,
+    });
 
-  await fastify.register(jwt, {
-    secret: 'nlwcopa',
-  })
+    await fastify.register(cors, {
+        origin: true,
+    });
 
-  await fastify.register(poolRoutes)
-  await fastify.register(authRoutes)
-  await fastify.register(gameRoutes)
-  await fastify.register(guessRoutes)
-  await fastify.register(userRoutes)
+    // em prod isso precisa ser uma variável de ambiente
+    await fastify.register(jwt, {
+        secret: 'nlwcopa',
+    });
 
-await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ })
+    await fastify.register(poolRoutes);
+    await fastify.register(userRoutes);
+    await fastify.register(guessRoutes);
+    await fastify.register(gameRoutes);
+    await fastify.register(authRoutes);
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
-bootstrap()
+boostrap();
